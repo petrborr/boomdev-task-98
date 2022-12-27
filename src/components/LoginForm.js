738c0b43573ec78d952217be93e60508360e1689
petrborr <petrborr@gmail.com>
2022-12-28 00:36:14 +0200

    add: form component with styles

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import formStyles from "./LoginForm.module.scss";
+
+export default function LoginForm() {
+    return (
+        <form className={formStyles.form}>
+            <label className={formStyles.label} for="name">Name</label>
+            <input className={formStyles.input} type="text" id="name" name="name"></input>
+            <label className={formStyles.label} for="password">Password</label>
+            <input className={formStyles.input} type="password" id="password" name="password"></input>
+            <input className={formStyles.submit} type="submit" value="Submit"></input>
+        </form>
+    )
+}
